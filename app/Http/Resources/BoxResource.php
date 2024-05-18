8a45941c771a549e083d3272380d05e0bea6465a
phpdeveloper2497 <142909633+phpdeveloper2497@users.noreply.github.com>
2024-05-18 16:49:41 +0500

    some change

--- app/Http/Resources/BoxResource.php
+++ app/Http/Resources/BoxResource.php
@@ -15,6 +15,7 @@ class BoxResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            "id" => $this->id,
             "name" =>$this->name,
             "per_liner_meter" =>$this->per_liner_meter,
             "remainder" =>$this->remainder,

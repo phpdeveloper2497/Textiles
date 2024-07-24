68497e93ccfe877b504cca1d56805706f531c3a0
phpdeveloper2497 <142909633+phpdeveloper2497@users.noreply.github.com>
2024-07-24 12:28:46 +0500

    changes

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::create([
+            'first_name' => 'admin',
+            'last_name' => 'admin',
+            'nickname' => 'admin',
+            'phone' => '+998945400807',
+            'password' => 'password'
+
+        ]);
+        $user->assignRole('admin');
+
+        $user = User::create([
+            'first_name' => 'warehouse',
+            'last_name' => 'manager',
+            'nickname' => 'john',
+            'phone' => '+99995719769',
+            'password' => 'password'
+        ]);
+        $user->assignRole('Ombor mudiri');
+//
+//        $user = User::create([
+//            'first_name' => 'case',
+//            'last_name' => 'manager',
+//            'nickname' => 'doe',
+//            'phone' => '',
+//            'password' => 'password'
+//        ]);
+//        $user->assignRole('Ish boshqaruvchi');
+    }
+}
